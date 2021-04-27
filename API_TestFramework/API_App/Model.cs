a3f6876407ad96a50fa3a7beb4035e5d8684551a
Martin Beard <mbeard@spartaglobal.com>
2021-04-27 14:43:36 +0100

    First Commit

---
+++ API_TestFramework/API_App/Model.cs
@@ -0,0 +1,61 @@
+﻿
+public class BulkPostcodeResponse
+{
+    public int status { get; set; }
+    public Result[] result { get; set; }
+}
+
+public class SinglePostcodeResponse
+{
+    public int status { get; set; }
+    public PostCode result { get; set; }
+}
+public class Result
+{
+    public string query { get; set; }
+    public PostCode result { get; set; }
+}
+
+public class PostCode
+{
+    public string postcode { get; set; }
+    public int quality { get; set; }
+    public int eastings { get; set; }
+    public int northings { get; set; }
+    public string country { get; set; }
+    public string nhs_ha { get; set; }
+    public float longitude { get; set; }
+    public float latitude { get; set; }
+    public string european_electoral_region { get; set; }
+    public string primary_care_trust { get; set; }
+    public string region { get; set; }
+    public string lsoa { get; set; }
+    public string msoa { get; set; }
+    public string incode { get; set; }
+    public string outcode { get; set; }
+    public string parliamentary_constituency { get; set; }
+    public string admin_district { get; set; }
+    public string parish { get; set; }
+    public string admin_county { get; set; }
+    public string admin_ward { get; set; }
+    public string ced { get; set; }
+    public string ccg { get; set; }
+    public string nuts { get; set; }
+    public Codes codes { get; set; }
+}
+
+public class Codes
+{
+    public string admin_district { get; set; }
+    public string admin_county { get; set; }
+    public string admin_ward { get; set; }
+    public string parish { get; set; }
+    public string parliamentary_constituency { get; set; }
+    public string ccg { get; set; }
+    public string ccg_id { get; set; }
+    public string ced { get; set; }
+    public string nuts { get; set; }
+    public string lsoa { get; set; }
+    public string msoa { get; set; }
+    public string lau2 { get; set; }
+}
---
+++ API_TestFramework/API_App/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace API_App
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var restClient = new RestClient("https://api.postcodes.io");
+            var restRequest = new RestRequest();
+            restRequest.Method = Method.GET;
+            restRequest.AddHeader("Content-Type", "application/json");
+            restRequest.Timeout = -1;
+
+            var postcode = "EC2Y 5AS";
+            restRequest.Resource = $"postcodes/{postcode.ToLower().Replace(" ", "")}";
+
+            var restResponse = restClient.Execute(restRequest);
+            Console.WriteLine("Response Content (string):");
+            Console.WriteLine(restResponse.Content);
+            Console.WriteLine("\n\n");
+
+            var jsonRespone = JObject.Parse(restResponse.Content);
+            Console.WriteLine("\nResponse content as a JObject");
+            Console.WriteLine(jsonRespone);
+            var adminDistrict = jsonRespone["result"]["admin_district"];
+            var paliamentaryConstituency = jsonRespone["result"]["paliamentary constituency"];
+            var adminWard = jsonRespone["result"]["admin_ward"];
+            Console.WriteLine(adminDistrict); 
+            Console.WriteLine(paliamentaryConstituency);
+            Console.WriteLine(adminWard);
+            var singlePostcodeResponse = JsonConvert.DeserializeObject<SinglePostcodeResponse>(restResponse.Content);
+            
+                
+                //var client = new RestClient("https://api.postcodes.io/postcodes/EC2Y5AS");
+            //client.Timeout = -1;
+            //var request = new RestRequest(Method.GET);
+            //request.AddHeader("Cookie", "__cfduid=d07ff89d6ceb11516cec847fedae7636c1619439989");
+            //IRestResponse response = client.Execute(request);
+            //Console.WriteLine(response.Content);
+
+            var client = new RestClient("https://api.postcodes.io/postcodes/");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Cookie", "__cfduid=d07ff89d6ceb11516cec847fedae7636c1619439989");
+            request.AddParameter("application/json", "{\r\n    \"postcodes\" : [\"OX49 5NU\", \"M32 0JG\", \"NE30 1DP\"]\r\n}", ParameterType.RequestBody);
+            IRestResponse response = await client.ExecuteAsync(request);
+            Console.WriteLine(response.Content);
+
+            var bulkJsonResponse = JObject.Parse(response.Content);
+
+            var bulkPostcodeJsonResponse = JsonConvert.DeserializeObject<BulkPostcodeResponse>(response.Content);
+
+            var adminDistrictFromBPR = bulkPostcodeJsonResponse.result[1].result.admin_district;
+
+            var quality = bulkPostcodeJsonResponse.result[0].result.quality;
+        }
+    }
+}
