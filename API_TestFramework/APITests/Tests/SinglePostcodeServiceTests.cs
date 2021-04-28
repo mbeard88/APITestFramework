3bd01531f6e9a590724f3db1c2353e912cdca95d
Martin Beard <mbeard@spartaglobal.com>
2021-04-28 09:33:32 +0100

    Async services

---
+++ API_TestFramework/APITests/Tests/SinglePostcodeServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using API_App;
+using NUnit.Framework;
+using System;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace APITests.Tests
+{
+    public class WhenTheSinglePostcodeServiceIsCalled_WithValidPostcode
+    {
+        SinglePostcodeService _singlePostcodeService;
+
+        [OneTimeSetUp]
+        public async Task OneTimeSetUp()
+        {
+            _singlePostcodeService = new SinglePostcodeService();
+            await _singlePostcodeService.MakeRequestAsync("EC2Y 5AS");
+        }
+
+        [Test]
+        public void StatusIs200()
+        {
+            Assert.That(_singlePostcodeService.ResponseContent["status"].ToString(), Is.EqualTo("200"));
+        }
+
+        [Test]
+        public void CorrectPostCodeIsReturned()
+        {
+            var result = _singlePostcodeService.ResponseContent["result"]["postcode"].ToString();
+            Assert.That(result, Is.EqualTo("EC2Y 5AS"));
+        }
+        [Test]
+        public void ObjectStatusIs200()
+        {
+            Assert.That(_singlePostcodeService.ResponseObject.status, Is.EqualTo(200));
+
+        }
+        [Test]
+        public void AdminDistrict_IsCityOfLondon()
+        {
+            Assert.That(_singlePostcodeService.ResponseObject.result.admin_district, Is.EqualTo("City of London"));
+        }
+
+    }
+}
---
+++ API_TestFramework/API_App/SinglePostcodeService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace API_App
+{
+    public class SinglePostcodeService
+    {
+        #region properties
+        public RestClient Client;
+        public IRestResponse RestResponse { get; set; }
+        public JObject ResponseContent { get; set; }
+        public SinglePostcodeResponse ResponseObject { get; set; }
+        public string PostcodeSelected { get; set; }
+
+        #endregion
+
+        // constructor - create the RestClient object
+        public SinglePostcodeService()
+        {
+            Client = new RestClient { BaseUrl = new Uri(AppConfigReader.BaseUrl) };
+        }
+        public async Task MakeRequestAsync(string postcode)
+        {
+            // Set request
+            var request = new RestRequest();
+            request.AddHeader("Content-Type", "application/json");
+            PostcodeSelected = postcode;
+
+            //define request resource path
+            request.Resource = $"postcodes/{postcode.ToLower().Replace(" ", "")}";
+
+            RestResponse = await Client.ExecuteAsync(request);
+
+            ResponseContent = JObject.Parse(RestResponse.Content);
+
+            // parse response body into an object tree
+            ResponseObject = JsonConvert.DeserializeObject<SinglePostcodeResponse>(RestResponse.Content);
+
+        }
+    }
+}
